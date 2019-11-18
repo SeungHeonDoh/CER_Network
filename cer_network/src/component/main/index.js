1b3a6e50c16c1fa9ddac5b9973298a88a5d01d7e
sangkwun <wer2774@gmail.com>
2019-11-18 15:42:56 +0900

    change config to use createConfig

--- cer_network/src/component/main/index.js
+++ cer_network/src/component/main/index.js
@@ -2,21 +2,13 @@ import React, { useState, useEffect } from 'react';
 import useNetwork from '../../hook';
 import D3Graph from '../../D3Graph/D3Graph';
 import dummy from '../../utils/data';
+import { createConfig } from '../../D3Graph/D3Graph.config';
 
 
 export default function Main() {
     var { nodes } = useNetwork();
     const [ loading, setLoading ] = useState(true);
-    const graphConfig = {
-        graph: {
-            staticGraph: false
-        },
-        isDarkTheme: false,
-        link: {},
-        node: {
-            renderLabel: true
-        }
-    }
+    const graphConfig = createConfig();
     const [ data, setData ] = useState(null);
     useEffect(() => {
         console.log(data);
